63977770ac6da19edc7dd4d27bb0b5f60cbdafec
Dens26 <denis.bekaert@live.fr>
2023-11-13 16:50:28 +0100

    v1.0

---
+++ assets/js/function.js
@@ -0,0 +1,78 @@
+
+/**
+ * Create key button
+ */
+export function createKeyButton(value) {
+    const key = document.createElement("input");
+    key.type = "button";
+    key.className = "key";
+    key.id = `key_${value}`;
+    key.name = `key_${value}`;
+    key.title = value < 10 ? `Touche ${value}` : "Touche vierge";
+    key.value = value < 10 ? value : "";
+    key.style.background = value < 10 ? "rgb(200, 200, 200)": "";
+    return key;
+}
+
+/**
+ * Create input 
+ */
+export function createInput(type, className, id, name, title, value, placeholder, disabled) {
+    const input = document.createElement("input");
+    input.type = type;
+    input.className = className;
+    input.id = id;
+    input.name = name;
+    input.title = title;
+    input.value = value;
+    input.placeholder = placeholder;
+    input.disabled = disabled;
+
+    return input;
+}
+
+/**
+ * Configure the keys event listener
+ */
+export function addKeyEventListener(keys, password) {
+    for (let i = 0; i < keys[0].children.length; i++) {
+        const key = keys[0].children[i];
+        key.addEventListener("click", () => {
+            if (key.value != "") {
+                if (password.value.length < 6) {
+                    password.value += key.value;
+                }
+            }
+        })
+    }
+    return password;
+}
+
+/**
+ * Try to log to the API
+ */
+export async function checkUser(user, password) {
+    try {
+        const data = {
+            login : `${user.value}`,
+            password : `${password.value}`
+        };
+
+        const response = await fetch(`https://www.ericfree.net/formation/api/check_user.php`, {
+            method : "POST",
+            headers : {
+                "Content-Type": "application/json"
+            },
+            body : JSON.stringify(data)
+        });
+        if (!response.ok) {
+            throw new Error(`Erreur ${response.status}`);
+        }
+        const result = await response.json();
+
+        return result.message;
+    }
+    catch (error) {
+        return error;
+    }
+}
---
+++ assets/js/main.js
@@ -0,0 +1,111 @@
+import { createKeyButton, createInput, addKeyEventListener, checkUser } from "./function.js"
+
+// DOM
+const mainForm = document.querySelector(".signin-form");
+
+// sign-in form
+const formContainer = document.createElement("form");
+formContainer.className = "form-container";
+formContainer.action = "#";
+
+// user div
+const userDiv = document.createElement("div");
+userDiv.className = "signin-group";
+
+// user label
+const userLabel = document.createElement("label");
+userLabel.htmlFor = "user";
+userLabel.textContent = "Identifiant";
+
+// user input
+userDiv.appendChild(userLabel);
+userDiv.appendChild(createInput("text", "input", "user", "user", "Identifiant", "", "Saisissez votre identifiant", false));
+
+// password div
+const passwordDiv = document.createElement("div");
+passwordDiv.className = "signin-group";
+
+// password label
+const passwordLabel = document.createElement("label");
+passwordLabel.htmlFor = "password";
+passwordLabel.textContent = "Mot de passe";
+
+// password input
+passwordDiv.appendChild(passwordLabel);
+passwordDiv.appendChild(createInput("password", "input", "password", "password", "Mot de passe", "", "Composez votre mot de passe", true));
+
+// numpadkey 
+let numpadKey = document.createElement("div");
+numpadKey.className = "numpad-key";
+
+// Create a key array and mix the value
+let keyTab = [];
+for (let i = 0; i < 16; i++)
+    keyTab.push(createKeyButton(i));
+
+const keyTabMixed = [...keyTab];
+
+for (let i = keyTabMixed.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [keyTabMixed[i], keyTabMixed[j]] = [keyTabMixed[j], keyTabMixed[i]];
+}
+for (let key of keyTabMixed)
+    numpadKey.appendChild(key);
+
+// form button
+const numpadButton = document.createElement("div");
+numpadButton.className = "numpad-button";
+numpadButton.appendChild(createInput("submit", "button", "submit", "submit", "Touche Valider", "Valider", "", false));
+numpadButton.appendChild(createInput("button", "button", "clear", "clear", "Touche Effacer", "Effacer", "", false));
+
+
+// Add all elements
+formContainer.appendChild(userDiv);
+formContainer.appendChild(passwordDiv);
+formContainer.appendChild(numpadKey);
+formContainer.appendChild(numpadButton);
+mainForm.appendChild(formContainer);
+
+// DOM references
+const keys = document.querySelectorAll(".numpad-key");
+let password = document.querySelector("#password");
+const user = document.querySelector("#user");
+const form = document.querySelector(".form-container");
+const errorDiv = document.querySelector(".error");
+const errorMsg = document.querySelector(".error-msg");
+
+// keys listener
+password = addKeyEventListener(keys, password);
+
+// Submit button form listener
+form.addEventListener("submit", async evt => {
+    evt.preventDefault();
+    errorMsg.textContent = "";
+    errorMsg.textContent = await checkUser(user, password);
+    errorDiv.style.display = "inherit";
+})
+
+// Clear button form listener
+form.addEventListener("click", (evt) => {
+    if (evt.target.id == "clear") {
+        numpadKey.textContent = "";
+        password.value = "";
+        errorDiv.style.display = "none";
+        errorMsg.textContent = "";
+
+        let keyTab = [];
+        for (let i = 0; i < 16; i++)
+            keyTab.push(createKeyButton(i));
+
+        const keyTabMixed = [...keyTab];
+
+        for (let i = keyTabMixed.length - 1; i > 0; i--) {
+            const j = Math.floor(Math.random() * (i + 1));
+            [keyTabMixed[i], keyTabMixed[j]] = [keyTabMixed[j], keyTabMixed[i]];
+        }
+        for (let key of keyTabMixed)
+            numpadKey.appendChild(key);
+
+        password = addKeyEventListener(keys, password);
+    }
+})
